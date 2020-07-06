dfa160ebc00ba59fd5811ca7a8835abce89cdb38
Jordi E <jordi@braingaze.eu>
2020-07-06 22:43:27 +0200

    added CMakeLists.txt

--- compile.sh
+++
@@ -1,2 +0,0 @@
-gcc test_c.c memmgr.c -o test_c.exe -pthread -fomit-frame-pointer -std=c99 -g -O0 -pipe -Wall -Wstrict-prototypes
-#g++ test_cpp.cpp memmgr.cpp -o test_cpp.exe -pthread -fomit-frame-pointer -g -O0  -std=c++11 -pipe -Wall
