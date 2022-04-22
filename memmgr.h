cbd074c886687bc042f9837e4f45b474d09de6d4
jespada <jespa007@gmail.com>
2022-04-22 20:45:13 +0200

    last changes

--- memmgr.h
+++ memmgr.h
@@ -61,12 +61,15 @@
 		void   		operator  delete(void  *_ptr)  _NO_EXCEPT_TRUE;
 		void   		operator  delete[](void  *_ptr)  _NO_EXCEPT_TRUE;
 
-
+#ifdef __APPLE__
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wkeyword-macro"
+#endif
 		#define	new		                                    (MEMMGR_push_file_line_new(__FILE__,__LINE__),false)?NULL:new
 		#define	delete		  		                        (MEMMGR_push_file_line_delete(__FILE__,__LINE__),false)?abort(): delete
+#ifdef __APPLE__
 #pragma GCC diagnostic pop
+#endif
 
 	#endif
 
