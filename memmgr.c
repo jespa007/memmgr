44d6ff36118bb482313636c40bd52cd2b2208a3e
jespada <jespa007@gmail.com>
2022-09-01 20:39:01 +0200

    last changes

--- memmgr.c
+++ memmgr.c
@@ -137,7 +137,7 @@ void  MEMMGR_get_filename(char  *filename, const char *absolute_filename)
 void MEMMGR_set_color_terminal(FILE *std_type, int attr, int fg, int bg)
 {
 
-	char command[13];
+	char command[50]={0};
 
 	/* Command is the control command to the terminal */
 	sprintf(command, "%c[%d;%d;%dm", 0x1B, attr, fg + 30, bg + 40);
